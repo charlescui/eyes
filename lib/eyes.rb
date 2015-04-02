2d1a226a3c7319fb65ae324b14d6b42f9e71d0e0
cuizheng <zheng.cuizh@gmail.com>
2015-04-02 19:49:52 +0800

    remove pry require

--- lib/eyes.rb
+++ lib/eyes.rb
@@ -2,7 +2,6 @@ $: << File.dirname(__FILE__)
 require "eyes/version"
 require "eyes/utils"
 require "m3u8"
-require "pry"
 
 require File.join(File.dirname(__FILE__), 'aliyun', 'connection')
 require File.join(File.dirname(__FILE__), 'eyes', 'watch_dog')
